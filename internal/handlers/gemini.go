704ac12cff1fc8161d92d0a6c415850ae134748a
unknown <novmitraa40@gmail.com>
2025-12-20 21:39:49 -0800

    change body style in screenshot

--- internal/handlers/gemini.go
+++ internal/handlers/gemini.go
@@ -186,6 +186,7 @@ func geminiMsgCreateHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 									<head>
 										<meta charset="UTF-8">
 										<meta name="viewport" content="width=device-width, initial-scale=1.0">
+										<style>body { margin: 0; padding: 0; display: inline-block; }</style>
 									</head>
 									<body>
 										%s
